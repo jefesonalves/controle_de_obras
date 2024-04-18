062eea9e5201b975aa6d8cdc0b23bf8d09368229
Jefeson <jefesonbezerra@gmail.com>
2024-04-18 16:23:41 -0300

    milhares na visualização do valor

--- cadastro/models.py
+++ cadastro/models.py
@@ -15,8 +15,7 @@ class Despesa(models.Model):
     data_despesa = models.DateField(verbose_name='Data')
     descricao_despesa = models.CharField(max_length=200, verbose_name='Descrição')
     nome_fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE, verbose_name='Fornecedor')
-    # fornecedor = models.CharField("Fornecedor", max_length=200)
-    valor_despesa = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Valor')
+    valor_despesa = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Valor')
 
     class Meta:
         verbose_name = 'Despesas'
